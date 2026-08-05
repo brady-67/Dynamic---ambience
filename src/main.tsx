8fc88f630ba5b668839ed5735f512450dbaf8ff5
brady-67 <bariga0015@stu.riarauniversity.ac.ke>
2026-08-05 20:01:52 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import AdminPage from './pages/Admin.tsx';
 import './index.css';
 
+const isAdminRoute = window.location.pathname.replace(/\/+$/, '') === '/admin';
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <StrictMode>{isAdminRoute ? <AdminPage /> : <App />}</StrictMode>
 );
