8f15e94aef979559c488f00642c859e6c3ff9129
brady-67 <bariga0015@stu.riarauniversity.ac.ke>
2026-08-21 12:06:45 +0300

    Update status.ts

--- api/mpesa/status.ts
+++ api/mpesa/status.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { supabaseServerClient } from '../_lib/daraja';
+import { supabaseServerClient } from '../_lib/daraja.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'GET') {
